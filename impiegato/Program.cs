5458de5af0386837c622c294d69e665c2241024b
Lore <Lore@DESKTOP-J005ERJ>
2024-08-14 14:39:42 +0200

    Aggiungere i file di progetto.

---
+++ impiegato/Program.cs
@@ -0,0 +1,14 @@
+﻿using impiegato;
+using static impiegato.Impiegato;
+
+
+class Program
+
+
+{
+    Impiegato impiegato = new Impiegato();
+    CalcolatoreStipendio calcolatoreStipendio = new CalcolatoreStipendio();
+
+    decimal stipendioAttuale = 3000m;
+    decimal percentualeIncremento = 5m; 
+}
---
+++ impiegato/impiegato.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impiegato
+{
+    public class Impiegato
+    {
+        public string nome;
+        public double stipendiomensile;
+        public decimal PercentualeIncremento; 
+        public DateTime dateTime;
+        public Impiegato()
+        {
+            nome = string.Empty;
+            dateTime = DateTime.Now;
+            stipendiomensile = 300;
+            PercentualeIncremento = 0; 
+        }
+        public class CalcolatoreStipendio
+        {
+            public static decimal IncrementaStipendio(decimal stipendiomensile, decimal percentualeIncremento)
+            {
+                decimal incremento = stipendiomensile * (percentualeIncremento / 100);
+                decimal nuovoStipendio = stipendiomensile + incremento;
+                return nuovoStipendio;
+            }
+        }
+      
+    }
+}
